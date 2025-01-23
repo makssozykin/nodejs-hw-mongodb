3861a293034d6d8e7865cab4f8db3a0ba920fde4
Maksym <mvsozykin@gmail.com>
2025-01-23 17:48:53 +0200

    http-errors istalled and errorHandler added

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -4,7 +4,7 @@ export const getContactsController = async (req, res) => {
   try {
     const contacts = await getAllContacts();
     res.status(200).json({
-      status: '200',
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -22,13 +22,13 @@ export const getContactByIdController = async (req, res) => {
     const contact = await getContactById(contactId);
     if (!contact) {
       res.status(404).json({
-        status: '404',
+        status: 404,
         message: 'Contact not found!',
       });
       return;
     }
     res.status(200).json({
-      status: '200',
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
