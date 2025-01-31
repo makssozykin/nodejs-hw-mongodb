0768db8e3490f4ab8ce6c3d7b20be1e990b36e8d
Maksym <mvsozykin@gmail.com>
2025-01-31 20:37:31 +0200

    validation contacts updated

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,15 +1,11 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  phoneNumber: Joi.string().min(3).max(30).required(),
-  email: Joi.string().min(3).max(30),
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
+  email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string()
-    .valid('work', 'home', 'personal')
-    .min(3)
-    .max(30)
-    .required(),
+  contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
 export const updateContactSchema = Joi.object({
@@ -17,5 +13,5 @@ export const updateContactSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(30),
   email: Joi.string().min(3).max(30),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').min(3).max(30),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
