0ab087a32826687401ac33fd82b5313766c702de
Maksym <mvsozykin@gmail.com>
2025-02-18 21:11:22 +0200

    all tasks done

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { UPLOAD_DIR } from './constants/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -14,6 +15,7 @@ export const startServer = () => {
   const app = express();
   app.use(express.json());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use(cors());
   app.use(cookieParser());
   app.use(
